17d6bdfef34afe184f4401a91931fb563921e751
AndreaL <alafertteo@gmail.com>
2018-11-14 12:53:17 -0300

    cambios en js

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ function screens() {
 
   document.getElementById('timer').style.display='none';
   document.getElementById('categorias').style.display='none';
+  document.getElementById('preguntaSeries').style.display='none';
+  document.getElementById('fin').style.display='none';
 
 };
 
@@ -20,7 +22,22 @@ function series() {
 
   document.getElementById('categorias').style.display ='none';
   document.getElementById('timer').style.display='block';
-}
+  document.getElementById('preguntaSeries').style.display='block';
+};
+
+/*let letter = document.getElementById("myInput").value;
+let score = 0;
+
+// If the letter is "c"
+if (value === "correcta") {
+    score ++;
+
+// If the letter is "b" or "d"
+} else  (value === "incorrecta") {
+    score = 0;
+};
+
+*/
 
 
 
@@ -32,3 +49,10 @@ let timeleft = 30;
   if(timeleft <= 0)
     clearInterval(downloadTimer);
   },1000);
+
+  /*
+  function sayHi() {
+    alert('Hello');
+  }
+  
+  setTimeout(sayHi, 3000);*/
